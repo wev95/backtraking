4080bacbb6dd06ae6277c40691e622f42bb79b4c
Weverton Leite Da Silva <weverton1995@hotmail.com>
2018-10-05 20:52:13 -0300

    Update backtraking_soma de quadrados.cpp

--- backtraking_soma de quadrados.cpp
+++ backtraking_soma de quadrados.cpp
@@ -9,19 +9,25 @@ void imprimir(int *V);
 
 void somaDeQuadrados(int *V, int n, int nq, int aux){
 	if(soma <= n){
-		if(soma == n)	imprimir(V);	
+		if(soma == n)	
+			imprimir(V);	
 		else{
 			for(int i = aux; i >= 1 && nq >= 0; i--){
-				V[nq] = i ;	soma += pow(i,2);	nq--;	
+				V[nq] = i ;	
+				soma += pow(i,2);	
+				nq--;	
 				somaDeQuadrados(V, n, nq, i);
-				nq++;	soma-= pow(i,2);	V[nq] = 0;	
+				nq++;	
+				soma-= pow(i,2);	
+				V[nq] = 0;	
 			}
 		}
 	}
 }
 void imprimir(int *V){
 	cout<<"{";		
-	for(int i = 0; i<=3 ; i++) cout<<V[i]<<" ";		
+	for(int i = 0; i<=3 ; i++) 
+		cout<<V[i]<<" ";		
 	cout<<"} ";
 }
 int main(){
