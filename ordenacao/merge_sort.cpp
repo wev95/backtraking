90c3bb12f85d5717c6e521337f2aa21b8bb3ddbb
Weverton Leite Da Silva <weverton1995@hotmail.com>
2022-07-10 14:06:03 -0300

    clean code mergesort

--- ordenacao/merge_sort.cpp
+++ ordenacao/merge_sort.cpp
@@ -3,38 +3,19 @@
 
 using namespace std;
 
+void mergeTheRestOfTheElements(vector<int>& N, vector<int>& partition, int& nPointer, int& partitionPointer, int sizePartition);
+void mergeLeftAndRight(vector<int>& N, int& nPointer, int endN, vector<int> Left, int& leftPointer, int leftSize, vector<int> Right, int& rightPointer, int rightSize);
+vector<int> subVector(vector<int> N, int start, int end, int rule);
 vector<int> merge(vector<int> N, int p, int half, int r);
 vector<int> mergeSort(vector<int> N, int start, int end);
 
-int main() {
-    vector<int> list = { 16, 12071, 2, 77, -1, 40, 5, 10, 37 };
-    int size = list.size() - 1;
-    vector<int> result = mergeSort(list, 0, size);
-
-    for (int n = 0; n <= size; ++n)
-        printf("%d ", result[n]);
-
-    return 0;
+void mergeTheRestOfTheElements(vector<int>& N, vector<int>& partition, int& nPointer, int& partitionPointer, int sizePartition) { 
+     while (partitionPointer < sizePartition) {
+        N[nPointer++] = partition[partitionPointer++];
+    }
 }
 
-vector<int> merge(vector<int> N, int startN, int halfN, int endN) {
-
-    vector<int> Left, Right;
-
-    int leftSize = halfN - startN + 1;
-    int rightSize = endN - halfN;
-    int leftPointer = 0, rightPointer = 0;
-
-    for (int i = 1; i <= leftSize; i++) {
-        Left.push_back(N[startN + i - 1]);
-    }
-
-    for (int i = 1; i <= rightSize; i++) {
-        Right.push_back(N[halfN + i]);
-    }
-
-    int nPointer = startN;
-
+void mergeLeftAndRight(vector<int>& N, int& nPointer, int endN, vector<int> Left, int& leftPointer, int leftSize, vector<int> Right, int& rightPointer, int rightSize) {
     while (leftPointer < leftSize && rightPointer < rightSize && nPointer <= endN) {
         if (Left[leftPointer] <= Right[rightPointer]) {
             N[nPointer] = Left[leftPointer++];
@@ -44,14 +25,32 @@ vector<int> merge(vector<int> N, int startN, int halfN, int endN) {
         }
         nPointer++;
     }
+}
 
-    while (leftPointer < leftSize) {
-        N[nPointer++] = Left[leftPointer++];
+vector<int> subVector(vector<int> N, int start, int end, int rule) {
+    vector<int> slice;
+
+    for (int i = start; i <= end; i++) {
+        slice.push_back(N[rule + i]);
     }
 
-    while (rightPointer < rightSize) {
-        N[nPointer++] = Right[rightPointer++];
-    }
+    return slice;
+}
+
+vector<int> merge(vector<int> N, int startN, int halfN, int endN) {
+
+    int leftSize = halfN - startN + 1;
+    int rightSize = endN - halfN;
+    int leftPointer = 0, rightPointer = 0;
+
+    vector<int> Left = subVector(N, 1, leftSize, startN - 1);
+    vector<int> Right = subVector(N, 1, rightSize, halfN);
+
+    int nPointer = startN;
+
+    mergeLeftAndRight(N, nPointer, endN, Left, leftPointer, leftSize, Right, rightPointer, rightSize);
+    mergeTheRestOfTheElements(N, Left, nPointer, leftPointer, leftSize);
+    mergeTheRestOfTheElements(N, Right, nPointer, rightPointer, rightSize);
 
     return N;
 }
@@ -67,4 +66,15 @@ vector<int> mergeSort(vector<int> N, int start, int end) {
         vector<int> mergeAB = merge(B, start, half, end);
         return mergeAB;
     }
+}
+
+int main() {
+    vector<int> list = { 16, 12071, 2, 77, -1, 40, 5, 10, 37 };
+    int size = list.size() - 1;
+    vector<int> result = mergeSort(list, 0, size);
+
+    for (int n = 0; n <= size; ++n)
+        printf("%d ", result[n]);
+
+    return 0;
 }
